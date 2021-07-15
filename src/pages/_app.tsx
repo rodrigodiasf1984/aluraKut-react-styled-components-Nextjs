0c6ea554976fae1e472e0ca23014a652deac333e
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-07-16 01:13:07 +0200

    chore: :art: added GlobalStyles and props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
-import "../../styles/globals.css";
+import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { theme, GlobalStyle } from '@/styles/globals';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
